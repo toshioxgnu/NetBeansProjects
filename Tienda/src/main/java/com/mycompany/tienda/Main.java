be8cd293a9951ddb55f77e4e3b91e77e9696ef15
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-11-12 17:17:25 -0300

    Adding Changues

--- Tienda/src/main/java/com/mycompany/tienda/Main.java
+++ Tienda/src/main/java/com/mycompany/tienda/Main.java
@@ -7,16 +7,16 @@ import java.util.*;
  * @author jose
  */
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    
-    static Queue <Integer> turno1 = new LinkedList<Integer>();
-    static Queue <Integer> turno2 = new LinkedList<Integer>();
 
-    
+    static Scanner sc = new Scanner(System.in);
+
+    static Queue<Integer> turno1 = new LinkedList<Integer>();
+    static Queue<Integer> turno2 = new LinkedList<Integer>();
+
     public static void main(String[] args) {
         int opcion;
-        
-        do{
+
+        do {
             System.out.println("Menu Ventas "
                     + "\n"
                     + "\n1.- Mostrar Ventas"
@@ -26,8 +26,8 @@ public class Main {
                     + "\n5.- Ventas Semana"
                     + "\n0.- Salir"
                     + "\nIngrese opcion: ");
-                    opcion = sc.nextInt();
-            switch(opcion){
+            opcion = sc.nextInt();
+            switch (opcion) {
                 case 1:
                     mostrarVentas();
                     break;
@@ -48,82 +48,82 @@ public class Main {
                 default:
                     System.out.println("Opcion Incorrecta");
             }
-            
-        }while(opcion!=0);
+
+        } while (opcion != 0);
     }
-    
-    
-    static void mostrarVentas(){
+
+    static void mostrarVentas() {
         System.out.println("Turno1:\n "
                 + turno1);
         System.out.println("Turno2:\n "
                 + turno2);
     }
-   static void agregarDatos(){
+
+    static void agregarDatos() {
         int turno, cantidad, dato;
         System.out.println("Ingrese turno: ");
         turno = sc.nextInt();
         System.out.println("Ingrese cantidad de datos a agregar");
         cantidad = sc.nextInt();
-        if(turno==1){
-            for (int i = 0; i<cantidad;i++){
+        if (turno == 1) {
+            for (int i = 0; i < cantidad; i++) {
                 System.out.println("Ingrese venta");
                 dato = sc.nextInt();
-                turno1.add(dato);   
+                turno1.add(dato);
             }
         }
-        if(turno==2){
-            for (int i = 0; i<cantidad;i++){
+        if (turno == 2) {
+            for (int i = 0; i < cantidad; i++) {
                 System.out.println("Ingrese venta");
                 dato = sc.nextInt();
-                turno2.add(dato);   
+                turno2.add(dato);
             }
         }
     }
-    static void ventaDiaria(){
+
+    static void ventaDiaria() {
         int venta = 0, dia = 0;
-        
-        for (Integer item: turno1){
-            for (Integer item2: turno2){
-		venta = item + item2;
-                System.out.println("Venta dia " + (dia+1) + " : "+ venta);
+
+        for (Integer item : turno1) {
+            for (Integer item2 : turno2) {
+                venta = item + item2;
+                System.out.println("Venta dia " + (dia + 1) + " : " + venta);
                 dia++;
             }
             break;
-	}
+        }
     }
-    static void ventasTotales(){
-        int t1 = 0 , t2 = 0;
-        
-        for (Integer item: turno1){
-            
-            for (Integer item2: turno2){
+
+    static void ventasTotales() {
+        int t1 = 0, t2 = 0;
+
+        for (Integer item : turno1) {
+
+            for (Integer item2 : turno2) {
                 t1++;
                 t2++;
             }
             break;
-	}
-        System.out.println("Ventas turno1 :"+ t1);
-        System.out.println("ventas turno2 :"+ t2);
+        }
+        System.out.println("Ventas turno1 :" + t1);
+        System.out.println("ventas turno2 :" + t2);
     }
-    static void ventaSemana(){
-        int ventaTurno1=0, ventaTurno2=0;
-        
-        for (Integer item: turno1){
-            
-            for (Integer item2: turno2){
-                ventaTurno1 =+ item;
-                ventaTurno2 =+ item2;
+
+    static void ventaSemana() {
+        int ventaTurno1 = 0, ventaTurno2 = 0;
+
+        for (Integer item : turno1) {
+
+            for (Integer item2 : turno2) {
+                ventaTurno1 = +item;
+                ventaTurno2 = +item2;
             }
             break;
-	}
-        System.out.println("Venta Turno 1: "+ ventaTurno1);
-        System.out.println("Venta Turno 2: "+ ventaTurno2);
-        System.out.println("Venta Total: "+ (ventaTurno1+ventaTurno2));
+        }
+        System.out.println("Venta Turno 1: " + ventaTurno1);
+        System.out.println("Venta Turno 2: " + ventaTurno2);
+        System.out.println("Venta Total: " + (ventaTurno1 + ventaTurno2));
 
     }
-    
-    
-    
-    
+
 }
